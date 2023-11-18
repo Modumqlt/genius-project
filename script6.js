460d5b7d5cf13cf7adbd66016ad49761d587d160
Modumqlt <helga.lemberg@gmail.com>
2023-11-18 18:08:34 +0200

    lesson6

---
+++ script6.js
@@ -0,0 +1,48 @@
+//***lesson6***
+
+//Створити телефонну книгу
+//створити початковий клас Abonent, де зберігатимуться ім'я і номер
+//створити set, який прийматиме телефон і номер
+//створити get, який виводитиме дані про абонента
+//створити три різні юзери
+//вивести дані
+
+//class Abonent {
+//    constructor(props) {
+//        this.name = props.name;
+//        this.tel = props.tel;
+    }
+
+//    firstName = "";
+//    lastName = "";
+//    tel = "";
+//    set name(newName) {
+//        const nameRow = newName.split(" ");
+//        this.firstName = nameRow[0];
+//        this.lastName = nameRow[1];
+//        var telRow = this.tel;
+//    }
+
+//    get name() {
+//        return `First name: ${this.firstName} last name: ${this.lastName} tel: ${this.tel}`;
+//    }
+//};
+
+//var Modumqlt = new Abonent({
+//    name: "Olha Maltseva",
+//    tel: "+380676812480"
+//});
+
+//var Svit = new Abonent({
+//    name: "Svitlana Skopova",
+//    tel: "+380984254561"
+//});
+
+//var Jan = new Abonent({
+//    name: "Ivan Korg",
+//    tel: "+380504269140"
+//});
+
+//console.log(Modumqlt);    
+//console.log(Svit); 
+//console.log(Jan);
