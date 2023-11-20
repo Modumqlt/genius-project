3fa4d345b2bb15161fcd2a68489346076117360d
Modumqlt <helga.lemberg@gmail.com>
2023-11-20 18:33:06 +0200

    lesson7

---
+++ script7.js
@@ -0,0 +1,28 @@
+//****lesson7****
+//Створіть функцію-конструктор Calculator, який створює об'єкти з трьома методами:
+//read() запитує два значення за допомогою prompt і запам'ятовує їх у властивостях об'єкта.
+//sum() повертає суму цих властивостей.
+//mul() повертає результат множення даних властивостей.
+
+//class Calculator {
+//    constructor() {
+//        this.x = 0;
+//        this.y = 0;
+//    }
+//    read() {
+//        this.x = prompt("введіть число x:");
+//        this.y = prompt("введіть число у:");
+//    }
+//    sum() {
+//        this.read();
+//        return console.log(Number(this.x) + Number(this.y));
+//    }
+//    mul() {
+//        return console.log(Number(this.x) * Number(this.y));
+//    }
+//}
+//calc = new Calculator();
+//calc.sum();
+//calc.mul();
+
+ 
