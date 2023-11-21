5c76c165260588ebf216789e264d1a30d1c063b2
Modumqlt <helga.lemberg@gmail.com>
2023-11-21 19:30:02 +0200

    lesson8

---
+++ script8.js
@@ -0,0 +1,6 @@
+//****lesson8****
+//Напишіть код, щоб вибрати елемент з атрибутом data-widget-name з документа та прочитати його значення
+
+    let element = document.querySelector("[data-widget-name]");    
+    console.log(element.getAttribute("data-widget-name"));
+   
