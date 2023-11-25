a9a8d54eb9c8b68081a9bf947fc4072547ee2e2b
Modumqlt <helga.lemberg@gmail.com>
2023-11-25 23:14:36 +0200

    lesson11

---
+++ lesson11/dist/main.js
@@ -0,0 +1,28 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/  var _webpack_modules_ = ({
+/***/ "./index.js":
+/*!*****************!*\
+  !*** ./index.js ***!
+  \******************/
+ /***/ ((_unused_webpack_module, _webpack_exports_, _webpack_require_) => {
+ 
+"use strict";
+eval("_webpack_require_.r(_webpack_exports_);\n/* harmony import */ var _module_WEBPACK_IMPORTED_MODULE_0__.name")
+
+/***/ }),
+
+/***/ "./module.js":
+/*!*******************!*\
+  !*** . 
+/******/ 	
+/******/ 	
+/******/ })()
+;
---
+++ lesson11/index.js
@@ -0,0 +1,15 @@
+//import {name} from "./module";
+//import "./style.css";
+
+//console.log("Your name: " + name);
+
+//import isEmail from 'validator/lib/isEmail';
+//import isUrl from 'validator/lib/isUrl';
+//import toDate from 'validator/lib/toDate';
+
+//console.log(isEmail('foo@bar.com'));
+//console.log(isUrl("https://woblink.com/account/moja-polka"));
+//console.log(toDate('12.12.2000'));
+var moment = require("moment");
+console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
+console.log(moment().format("MMM Do YY"));
